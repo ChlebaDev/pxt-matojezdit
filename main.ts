aa022466ae05c906767861a60240c27be7a5519f
ChlebaDev <115696170+ChlebaDev@users.noreply.github.com>
2023-05-10 22:04:23 +0200

    zkouska

--- main.ts
+++ main.ts
@@ -1,7 +1,12 @@
 // Máme mít sestavu abychom věděli že auto jezdí, nevím jestli použít senzory
 // "XYABLO12" mohu poslat např. "@_______" => bude osa x 64
 // x  = -128 - 127, y = -128 - 127, A B = 0, 1
-    
+  
+radio.setGroup(12)
+radio.setTransmitSerialNumber(true)
+radio.setFrequencyBand(12)
+radio.setTransmitPower(7)
+
 function autickoJede(lw: number = 0, rw: number = 0) {
     const ul = Math.map(lw, -100, 100, -235, 235) // přizpůsobit našem kolům
     const ur = Math.map(rw, -100, 100, -255, 255)
@@ -26,9 +31,14 @@ function autickoDoleva() {
 function autickoDoprava() {
     autickoJede(0, 100)
 }
+/*
 
-function sestava() {
-    
+let cas = 
+
+function sestava() { // 2 sekundy dopředu, poté zastavit a 1 sek doleva zastavit a sekundu dozadu, poté sekundu doprava dozadu a zpět rovně 3 sekundy
+    while () {
+        
+    }
 }
 
 
@@ -37,10 +47,39 @@ radio.onReceivedString(function (receivedString: string) {
     return 
 })
 
+*/
+
+radio.onReceivedNumber(function(receivedNumber: number) {
+    PCAmotor.MotorRun(PCAmotor.Motors.M1, 250)
+    PCAmotor.MotorRun(PCAmotor.Motors.M4, -215)
+    basic.pause(2000)
+    PCAmotor.MotorRun(PCAmotor.Motors.M1, 50)
+    PCAmotor.MotorRun(PCAmotor.Motors.M4, -200)
+    basic.pause(1000)
+    PCAmotor.MotorRun(PCAmotor.Motors.M1, 200)
+    PCAmotor.MotorRun(PCAmotor.Motors.M4, -50)
+    basic.pause(1000)
+    PCAmotor.MotorRun(PCAmotor.Motors.M1, -220)
+    PCAmotor.MotorRun(PCAmotor.Motors.M4, 200)
+    basic.pause(2000)
+    PCAmotor.MotorRun(PCAmotor.Motors.M1, 250)
+    PCAmotor.MotorRun(PCAmotor.Motors.M4, 250)
+    basic.pause(1000)
+    PCAmotor.MotorRun(PCAmotor.Motors.M1, 20)
+    PCAmotor.MotorRun(PCAmotor.Motors.M4, -250)
+    basic.pause(5000)
+    PCAmotor.MotorStopAll()
+})
 
 
 
-
-input.onButtonPressed(Button.AB, function() {
-    input.acceleration(Dimension.X)
+radio.onReceivedNumber(function(receivedNumber: number) {
+    console.log(receivedNumber)
+    basic.showLeds(`
+    # . . . #
+    . . . . .
+    . . # . .
+    . . . . .
+    # . . . #
+    `)
 })
